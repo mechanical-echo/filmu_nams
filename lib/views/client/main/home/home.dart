4b9f6e9122ce279e496203714596d4c7f569afdc
Sofia Dyshlovaya <sofija45642@gmail.com>
2025-02-24 11:45:03 +0200

    wip | carousel

--- lib/views/client/main/home/home.dart
+++ lib/views/client/main/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:filmu_nams/assets/decorations/stylized_carousel.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,11 +11,31 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Center(
-      child: Container(
-        width: 100,
-        height: 100,
-        color: Colors.deepOrange,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: width,
+            height: height * 0.3,
+            child: StylizedCarousel(
+              items: List.generate(
+                5,
+                (index) => Container(
+                  decoration: BoxDecoration(
+                    color: Colors.primaries[index % Colors.primaries.length],
+                  ),
+                  alignment: Alignment.center,
+                  child: Text("Item $index",
+                      style: TextStyle(fontSize: 24, color: Colors.white)),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
