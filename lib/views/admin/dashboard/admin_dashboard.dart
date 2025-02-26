6f7a1121d188bd47fcbd61e68e47fc4f645c540d
Sofia Dyshlovaya <sofija45642@gmail.com>
2025-02-26 18:47:45 +0200

    fix

--- lib/views/admin/dashboard/admin_dashboard.dart
+++ lib/views/admin/dashboard/admin_dashboard.dart
@@ -1,8 +1,8 @@
 import 'package:filmu_nams/assets/decorations/background.dart';
 import 'package:filmu_nams/views/admin/dashboard/widgets/expandable_view/expandable_view.dart';
 import 'package:filmu_nams/views/admin/dashboard/widgets/homepage_carousel.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AdminDashboard extends StatefulWidget {
   const AdminDashboard({super.key});
@@ -14,38 +14,113 @@ class AdminDashboard extends StatefulWidget {
 class _AdminDashboardState extends State<AdminDashboard> {
   @override
   Widget build(BuildContext context) {
-    return Background(
-      child: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Center(
-                child: Column(
-                  spacing: 20,
-                  children: [
-                    HomepageCarousel(),
+    double height = MediaQuery.of(context).size.height;
 
-                    // Placeholders
-                    ExpandableView(title: "Filmas", child: Placeholder()),
-                    ExpandableView(title: "Lietotāji", child: Placeholder()),
-                    ExpandableView(title: "Piedāvājumi", child: Placeholder()),
-                  ],
+    return Background(
+      child: Row(
+        children: [
+          Container(
+            height: height,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+              border: Border(
+                right: BorderSide(
+                  color: Colors.white.withAlpha(50),
+                  width: 1,
+                ),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withAlpha(50),
+                  blurRadius: 10,
+                  offset: const Offset(0, 0),
+                ),
+              ],
+            ),
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Positioned(
+                  top: 10,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).focusColor,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withAlpha(50),
+                          blurRadius: 10,
+                          offset: const Offset(0, 10),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          spacing: 8,
+                          children: [
+                            Icon(
+                              Icons.movie_filter,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                            Text(
+                              "Filmu Nams",
+                              style: GoogleFonts.poppins(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                        Text(
+                          "Administrācijas panelis",
+                          style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context)
+                                .bottomNavigationBarTheme
+                                .unselectedItemColor,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 20,
+                    children: [
+                      HomepageCarousel(),
+
+                      // Placeholders
+                      ExpandableView(title: "Filmas", child: Placeholder()),
+                      ExpandableView(title: "Lietotāji", child: Placeholder()),
+                      ExpandableView(
+                          title: "Piedāvājumi", child: Placeholder()),
+                    ],
+                  ),
                 ),
               ),
             ),
-            Positioned(
-              top: 20,
-              right: 20,
-              child: FilledButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: Text("Izlogoties"),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
