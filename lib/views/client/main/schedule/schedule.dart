1fa6d7f6ea8b6c4309806c127224fef0f22949da
Sofia Dyshlovaya <sofija45642@gmail.com>
2025-02-28 09:49:03 +0200

    better design for movie items

--- lib/views/client/main/schedule/schedule.dart
+++ lib/views/client/main/schedule/schedule.dart
@@ -4,6 +4,7 @@ import 'package:filmu_nams/controllers/movie_controller.dart';
 import 'package:filmu_nams/views/client/auth/login/login.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class Schedule extends StatefulWidget {
   const Schedule({super.key});
@@ -90,10 +91,9 @@ class _MovieListState extends State<MovieList> {
       child: GridView.count(
         padding:
             const EdgeInsets.only(top: 20, bottom: 70, left: 10, right: 10),
-        crossAxisCount: 2,
-        childAspectRatio: 0.58,
+        crossAxisCount: 1,
+        childAspectRatio: 1.5,
         mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
         children: movieData != null
             ? List.generate(
                 movieData!.length,
@@ -117,72 +117,178 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.topCenter,
-      children: [
-        Positioned(
-          bottom: 20,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            width: 165,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10),
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        color: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Colors.black.withAlpha(100),
+                blurRadius: 10,
+                offset: const Offset(5, 0),
               ),
+            ]),
+            child: Stack(
+              alignment: Alignment.topRight,
+              children: [
+                poster(),
+                rating(context),
+              ],
             ),
-            child: Center(
-              child: Text(
-                data['title']!,
-                style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w600,
-                ),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-              ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                title(context),
+                description(context),
+                button(context),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Container button(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.black.withAlpha(80),
+          blurRadius: 10,
+          offset: const Offset(0, 5),
+        ),
+      ]),
+      child: Center(
+        child: FilledButton(
+          onPressed: () {},
+          style: FilledButton.styleFrom(
+            padding: const EdgeInsets.all(0),
+            backgroundColor: Theme.of(context).focusColor,
+            fixedSize: Size(200, 30),
+          ),
+          child: Text(
+            "Vairāk",
+            style: GoogleFonts.poppins(
+              fontSize: 16,
             ),
           ),
         ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-          decoration: BoxDecoration(
-            color: Theme.of(context).focusColor,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withAlpha(120),
-                blurRadius: 10,
-                offset: const Offset(0, 7),
-              ),
-            ],
+      ),
+    );
+  }
+
+  Container poster() {
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      width: 190,
+      decoration: BoxDecoration(),
+      child: CachedNetworkImage(
+        imageUrl: data['poster-url']!,
+        fit: BoxFit.cover,
+        placeholder: (context, url) => Center(
+          child: LoadingAnimationWidget.staggeredDotsWave(
+            color: Colors.white,
+            size: 100,
           ),
-          child: Container(
-            clipBehavior: Clip.antiAlias,
-            height: 250,
-            width: 170,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withAlpha(225),
-                  blurRadius: 8,
-                  offset: const Offset(0, 5),
-                ),
-              ],
-            ),
-            child: CachedNetworkImage(
-              imageUrl: data['poster-url']!,
-              fit: BoxFit.cover,
-              placeholder: (context, url) => CircularProgressIndicator(),
-            ),
+        ),
+      ),
+    );
+  }
+
+  Container rating(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 10,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          bottomLeft: Radius.circular(10),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(100),
+            blurRadius: 10,
+            offset: const Offset(-5, 0),
+          )
+        ],
+        color: Theme.of(context).focusColor,
+      ),
+      child: Text(
+        data['rating']!,
+      ),
+    );
+  }
+
+  Container description(BuildContext context) {
+    return Container(
+      height: 125,
+      margin: const EdgeInsets.only(left: 15, right: 15, bottom: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+      decoration: BoxDecoration(
+          color: Theme.of(context).focusColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+            bottomRight: Radius.circular(10),
           ),
-        )
-      ],
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha(60),
+              blurRadius: 8,
+              offset: const Offset(0, 7),
+            ),
+          ]),
+      child: Text(
+        data['description']!,
+        maxLines: 5,
+        overflow: TextOverflow.ellipsis,
+        style: GoogleFonts.poppins(
+          color: Colors.white,
+          fontSize: 15,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    );
+  }
+
+  Container title(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+      decoration: BoxDecoration(
+          color: Theme.of(context).focusColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha(60),
+              blurRadius: 8,
+              offset: const Offset(0, 7),
+            ),
+          ]),
+      child: Text(
+        data['title']!,
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+        style: GoogleFonts.poppins(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
     );
   }
 }
